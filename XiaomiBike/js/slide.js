a5a1f37dd3dae6af573bb9e3927a89f0edd73ba2
Bogdan-Wdev <121345087+Bogdan-Wdev@users.noreply.github.com>
2022-12-24 15:12:07 +0200

    Test project

---
+++ XiaomiBike/js/slide.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.slider-block__image , .fullscreen__slider').slick({
+        arrows: false,
+        dots:   true,
+        fade:   true,
+        autoplay: true,
+        autoplaySpeed: 1500
+        
+      });
+      
+
+});
